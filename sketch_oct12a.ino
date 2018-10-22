1ac239d92e49d322df8d07686501a17f7582d581
ctliew <ctliew2@gmail.com>
2018-10-22 18:45:35 -0500

    Add files via upload

---
+++ sketch_oct12a.ino
@@ -0,0 +1,16 @@
+void setup() {
+  Serial.begin(9600); //baud rate
+  Serial.print("Your code is working!");
+
+  pinMode(11, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  for( int i = 0 ; i < 256 ; i++){
+    analogWrite(11, i);
+    delay(50);
+  }
+
+}
